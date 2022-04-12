81080fcdedc82291778493079209cff195a1e57c
kauefraga <kauefragarodrigues456@gmail.com>
2022-04-11 23:17:17 -0300

    :label: types: add viacep api response

---
+++ src/types/ViacepAPIResponse.ts
@@ -0,0 +1,10 @@
+interface ViacepAPIResponse {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ddd: string;
+}
+
+export default ViacepAPIResponse;

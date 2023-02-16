f175343ae420f02f373d8e4e03afe0af85b3fe54
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-02-16 17:27:15 -0300

    :recycle: chore: tranform classes in functions

--- src/components/answer.ts
+++ src/components/answer.ts
@@ -1,26 +1,12 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-import ViacepAPIResponse from '../types/ViacepAPIResponse';
+import ViacepAPIResponse from '../types/viacep-api-response';
 
-class Answer {
-  constructor(
-    public data: ViacepAPIResponse,
-  ) {
-    this.data = data;
-  }
-
-  // TODO: for...in loop
-  showIt() {
-    // eslint-disable-next-line no-console
-    console.log({
-      cep: this.data.cep,
-      logradouro: this.data.logradouro,
-      bairro: this.data.bairro,
-      localidade: this.data.localidade,
-      uf: this.data.uf,
-      ddd: this.data.ddd,
-    });
-  }
+export function showData(data: ViacepAPIResponse) {
+  return console.log({
+    cep: data.cep,
+    logradouro: data.logradouro,
+    bairro: data.bairro,
+    localidade: data.localidade,
+    uf: data.uf,
+    ddd: data.ddd,
+  });
 }
-
-export default Answer;
--- src/components/logo.ts
+++ src/components/logo.ts
@@ -1,21 +1,10 @@
 import chalk from 'chalk';
 import figlet from 'figlet';
 
-class Logo {
-  constructor(
-    public name: string,
-  ) {
-    this.name = name;
-  }
-
-  showIt() {
-    // eslint-disable-next-line no-console
-    console.log(
-      chalk.blueBright(
-        figlet.textSync(this.name, { horizontalLayout: 'fitted' }),
-      ),
-    );
-  }
+export function showLogo(name: string) {
+  console.log(
+    chalk.blueBright(
+      figlet.textSync(name, { horizontalLayout: 'fitted' }),
+    ),
+  );
 }
-
-export default Logo;
